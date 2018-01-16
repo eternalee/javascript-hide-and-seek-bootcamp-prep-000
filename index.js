048f1d151d3cab167841c3c9ebf6bad3b65589ec
Eterna <one.eternity@gmail.com>
2018-01-16 10:11:12 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,18 @@
+function getFirstSelector(selector) {
+  return document.querySelector(selector)
+}
+
+function nestedTarget() {
+  return document.querySelector('#nested .target')
+}
+
+function increaseRankBy(n) {
+  const x = document.querySelectorAll('.ranked-list li')
+  for (let i=0; i < x.length; i++) {
+    x[i].innerHTML = parseInt(x[i].innerHTML) + n
+  }
+}
+
+function deepestChild() {
+  
+}
